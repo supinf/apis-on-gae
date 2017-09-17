6c0fc858b10f8644c81bd800d0360cb72673f636
ryo nakamaru <pottava@gmail.com>
2017-09-18 03:32:51 +0900

    swagger.yaml & generated source code

---
+++ api/restapi/doc.go
@@ -0,0 +1,23 @@
+/*Package restapi demo-apis
+
+GCPUG Tokyo DevOps Day September 2017 Demo API
+
+
+
+    Schemes:
+      http
+    Host: localhost
+    BasePath: /api/v1
+    Version: 1.0.0
+
+    Consumes:
+    - application/json
+
+
+    Produces:
+    - application/json
+
+
+swagger:meta
+*/
+package restapi
